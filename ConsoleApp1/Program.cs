d2031da28ced29341e5bf731b7a5a416f2358c8e
Grace Looney <gralloon@ut.utm.edu>
2020-01-16 09:34:31 -0600

    A custom guessing range feature was created minor grammer issues fixed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,15 +10,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int myNum;
+            int myNum, begin, end;
             int theirNum;
             string userInput;
-            myNum = new Random().Next(0, 100);
+            
+            Console.WriteLine("Hello World");
+
+
+            //determining range
+            Console.WriteLine("What's the range of the guessing? [x,y]");
+            Console.Write("x?");
+            userInput = Console.ReadLine();
+            begin = Convert.ToInt32(userInput);
+            Console.Write("y?");
+            userInput = Console.ReadLine();
+            end = Convert.ToInt32(userInput);
+
+            //thinking of Number
+            myNum = new Random().Next(begin, end);
             int rNum = myNum;
             Console.WriteLine(rNum);
-            Console.WriteLine("Hello World");
-            Console.WriteLine("I am thinking of a number between 0 and 100");
-            Console.WriteLine("What your guess?");
+
+            Console.WriteLine(userInput);
+            Console.WriteLine("I am thinking of a number between " + begin + "and" + end);
+
+            Console.WriteLine("What is your guess?");
             userInput = Console.ReadLine();
             theirNum = Convert.ToInt32(userInput);
             while (theirNum != myNum)
@@ -26,14 +42,14 @@ namespace ConsoleApp1
                 if (theirNum > myNum)
                 {
                     Console.WriteLine(theirNum + "is higher than my number");
-                    Console.WriteLine("What your new guess?");
+                    Console.WriteLine("What is your new guess?");
                     userInput = Console.ReadLine();
                     theirNum = Convert.ToInt32(userInput);
                 }
                 else if (theirNum < myNum)
                 {
                     Console.WriteLine( theirNum + " is lower than my number");
-                    Console.WriteLine("What your new guess?");
+                    Console.WriteLine("What is your new guess?");
                     userInput = Console.ReadLine();
                     theirNum = Convert.ToInt32(userInput);
                 }
